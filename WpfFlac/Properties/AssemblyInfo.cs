cf1d5310d012e4cf32069ff1c8dfe1b5582e9f0d
Nongorilla <Nongorilla@users.noreply.github.com>
2018-06-12 18:47:12 -0700

    rev to 4.1.0.1

--- WpfFlac/Properties/AssemblyInfo.cs
+++ WpfFlac/Properties/AssemblyInfo.cs
@@ -12,4 +12,4 @@ using System.Windows;
 [assembly: AssemblyCulture ("")]
 [assembly: ComVisible (false)]
 [assembly: ThemeInfo (ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion ("4.1.0.0")]
+[assembly: AssemblyVersion ("4.1.0.1")]
